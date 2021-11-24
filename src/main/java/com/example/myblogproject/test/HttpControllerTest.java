0f84084b1f5730355a28d8c526808539ade03161
shinequasar <shinequasar@gmail.com>
2021-11-24 18:23:52 +0900

    [feat] lombok 세팅 및 사용

--- src/main/java/com/example/myblogproject/test/HttpControllerTest.java
+++ src/main/java/com/example/myblogproject/test/HttpControllerTest.java
@@ -9,20 +9,31 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HttpControllerTest {
 
+    private static final String TAG = "HttpControllerTest : ";
+
+    @GetMapping("/http/lombok")
+    public String lombokTest(){
+        Member m = new Member.MemberBuilder().username("want").password("1234").email("email").build();
+        System.out.println(TAG+"getter : "+m.getUsername());
+        m.setUsername("want");
+        System.out.println(TAG+"setter : "+m.getUsername());
+        return "lombok test 완료";
+    }
+
     //select
     @GetMapping("/http/get")
-    public String getTest(){
-        return "get 요청";
+    public String getTest(Member m){
+        return "get 요청 : "+m.getId()+" , "+m.getUsername()+" , "+m.getPassword();
     }
     //instert
     @PostMapping("/http/post")
-    public String pustTest(){
-        return "pust 요청";
+    public String pustTest(@RequestBody Member m){  //MessageConverter(스프링부트)
+        return "post 요청 : "+ m.getId()+" , "+m.getUsername()+" , "+m.getPassword();
     }
     //update
     @PutMapping("/http/put")
-    public String putTest(){
-        return "put 요청";
+    public String putTest(@RequestBody Member m){
+        return "put 요청 : "+ m.getId()+" , "+m.getUsername()+" , "+m.getPassword();
     }
     //delete
     @DeleteMapping("/http/delete")
