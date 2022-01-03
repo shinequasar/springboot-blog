2cab01c60b9ad5aa287f3eb18fcebd6b014c8fab
shinequasar <shinequasar@gmail.com>
2022-01-03 12:54:31 +0900

    [feat] select 시  optional로 인자에러처리

--- src/main/java/com/example/myblogproject/test/DummyControllerTest.java
+++ src/main/java/com/example/myblogproject/test/DummyControllerTest.java
@@ -4,14 +4,31 @@ import com.example.myblogproject.model.RoleType;
 import com.example.myblogproject.model.User;
 import com.example.myblogproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.function.Supplier;
 
 @RestController
 public class DummyControllerTest {
 
     @Autowired //의존성 주입
     private UserRepository userRepository;
+
+    @GetMapping("/dummy/user/{id}")
+    public User detail(@PathVariable Long id){
+        //만약 id 파라메터가 잘못되어 null이 반환되면 X.
+        //Optional로 가져와 판단해 리턴하기
+        User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>(){
+            @Override
+            public IllegalArgumentException get() {
+                return new IllegalArgumentException("해당 유저는 존재하지 않습니다. Id : " +id);
+            }
+        });
+        return user;
+    }
 
     @PostMapping("/dummy/join")
     public String join(User user){ //key = value(약속된 규칙)
