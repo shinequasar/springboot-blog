a1104799c37ed2f338407745c489babbdf0683a2
shinequasar <shinequasar@gmail.com>
2022-01-24 15:14:03 +0900

    [feat] 글목록 페이징 처리

--- src/main/java/com/example/myblogproject/controller/BoardController.java
+++ src/main/java/com/example/myblogproject/controller/BoardController.java
@@ -2,6 +2,9 @@ package com.example.myblogproject.controller;
 
 import com.example.myblogproject.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +16,9 @@ public class BoardController {
     private BoardService boardService;
 
     @GetMapping("/")
-    public String index(Model model){  // model은 jsp에서 request정보같은 거. model을 가지고 index까지 가져감
-        model.addAttribute("boards", boardService.글목록());
+    public String index(Model model,
+                        @PageableDefault(size=3, sort = "id", direction= Sort.Direction.DESC)Pageable pageable){  // model은 jsp에서 request정보같은 거. model을 가지고 index까지 가져감
+        model.addAttribute("boards", boardService.글목록(pageable));
         //WEB-INF/views/index.jsp
         return "index";  // viewResolver 작동
     }
